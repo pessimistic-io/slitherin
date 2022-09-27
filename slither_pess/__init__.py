29913f283b7d4f13f851c9e44058f681651b9b1f
nikitaka7 <nikitaka7@mail.ru>
2022-09-27 11:35:32 +0700

    Strange setter setup

--- slither_pess/__init__.py
+++ slither_pess/__init__.py
@@ -1,9 +1,10 @@
 from slither_pess.detectors.double_entry_token_possibility import DoubleEntryTokenPossiblity
+from slither_pess.detectors.strange_setter import StrangeSetter
 from slither_pess.detectors.unprotected_setter import UnprotectedSetter
 
 
 def make_plugin():
-    plugin_detectors = [DoubleEntryTokenPossiblity,UnprotectedSetter]
+    plugin_detectors = [DoubleEntryTokenPossiblity,UnprotectedSetter, StrangeSetter]
     plugin_printers = []
 
     return plugin_detectors, plugin_printers
