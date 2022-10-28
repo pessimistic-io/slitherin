8f4e4264d315a3fa1e007606580ab24795e9164a
ndkirillov <nikitch0905@gmail.com>
2022-10-28 14:14:14 +0700

    function in test removed

--- tests/dubious_typecast_test.sol
+++ tests/dubious_typecast_test.sol
@@ -1,13 +1,6 @@
 pragma solidity ^0.8.0;
 
 contract dubious_typecast_test {
-    uint256 constant a = 1000;
-    uint8 constant b = 10;
-
-    function mul_vulnerable_uint () public pure returns (uint8){
-        uint8 res = uint8(a)*b;
-        return res;
-    }
 
     function getOne () public pure returns (bytes32){
         return bytes32(bytes1(0x01));
