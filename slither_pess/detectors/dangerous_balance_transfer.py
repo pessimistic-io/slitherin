9f6d42bfabd15c3887415b9218da2923fbef2cf2
nikitaka7 <nikitaka7@mail.ru>
2022-11-03 14:04:27 +0700

    dangerous balance transfer modification

--- slither_pess/detectors/dangerous_balance_transfer.py
+++ slither_pess/detectors/dangerous_balance_transfer.py
@@ -1,11 +1,11 @@
-from slither.core.declarations import SolidityVariableComposed, SolidityFunction, SolidityVariable
+from pyclbr import Function
+from typing import List
 from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
-from slither.analyses.data_dependency.data_dependency import is_dependent
 
 
 class DangerousBalanceTransfer(AbstractDetector):
     """
-    Sees if contract contains a transfer of 90%+ contract balance
+    Sees if contract contains a transfer of a contract balance
     """
 
     ARGUMENT = 'dangerous-balance-transfer' # slither will launch the detector with slither.py --detect mydetector
@@ -19,32 +19,37 @@ class DangerousBalanceTransfer(AbstractDetector):
     WIKI_EXPLOIT_SCENARIO = 'Contract transfers all its balance to a malicious address'
     WIKI_RECOMMENDATION = 'Be careful with contract balance transfers'
 
-
-    def is_dangerous_balance_transfer(self, fun):
-        for n in fun.nodes: # в первом приближении нода это строчка
+    def is_dangerous_balance_transfer(self, fun) -> str:
+        _signatures = ["transfer(address,uint256)","transferFrom(address,address,uint256)","safeTransfer(IERC20,address,uint256)","safeTransferFrom(IERC20,address,address,uint256)"]
+        for n in fun.nodes:
             for ir in n.irs:
-                if(ir.function.solidity_signature == "transfer(address,uint256)"):
-                    if(str(ir.expression).__contains__('transfer')):
-                        expressionString = str(ir.expression)
-                        if(expressionString.__contains__('balanceOf' or 'this')):
+                if ir.function.solidity_signature in _signatures:
+                        if not self.has_access_control(fun) and str(n).__contains__("balanceOf" or "this"):
                             return "True"
-                        #TODO Relations to balanceOf(address(this))
-                        # print(expressionString)
-                        # indx1 = expressionString.index(',')
-                        # indx2 = expressionString.index(')')
-                        # res = ''
-                        # for idx in range(indx1 + len(','), indx2):
-                        #     res = res + expressionString[idx]
-                        # if(is_dependent(res, SolidityFunction("balance(address)"), n.function.contract)):
-                        #     return "True"
-                #if(expressionString)
-                    #print(ir.function)
-                    # print(ir.expression)
-                    # return "True"
-
+                        if not self.has_access_control(fun) and self.amount_is_not_limited(fun):
+                            return "True"
         return "False"
 
-    def _detect(self):
+    def has_access_control(self, fun: Function) -> bool:
+        for m in fun.modifiers:
+            for m.name in ['initializer', 'onlyOwner']:
+                return True
+
+    def amount_is_not_limited(self, fun: Function) -> bool:
+        for function_parameter in fun.parameters:
+            if str(function_parameter.type) == "uint256":
+                num_changes = 0
+                for n in fun.nodes:
+                    print(n)
+                    print(function_parameter)
+                    if str(n).__contains__(str(function_parameter)):
+                        num_changes = num_changes+1
+                    if num_changes >= 2:
+                        return False
+        print(fun)
+        return True
+
+    def _detect(self) -> List:
 
         res = []
 
@@ -53,8 +58,7 @@ class DangerousBalanceTransfer(AbstractDetector):
                 x = self.is_dangerous_balance_transfer(f)
                 if (x == "True"):
                     res.append(self.generate_result([
-                        f.contract_declarer.name, ' ',
-                        f.name, ' has a dangerous balance transfer with dependence on balanceOf contract'
+                        f, ' can transfer contract balance'
                         '\n']))
 
 
--- tests/dangerous_balance_transfer_test.sol
+++ tests/dangerous_balance_transfer_test.sol
@@ -1,11 +1,17 @@
 /*
 Warning! The code is not production ready. It is provided for educational purposes only.
 */
-
 pragma solidity ^0.8.0;
+import "./interfaces/IERC20.sol";
 
-contract ERC20 {
+contract DangerousERC20 {
     mapping(address => uint256) balances;
+    bool isProtected = true;
+
+    modifier onlyOwner() {
+        require(isProtected);
+        _;
+    }
 
     event Transfer(address indexed from, address indexed to, uint256 value);                
 
@@ -25,13 +31,20 @@ contract ERC20 {
     function transfer_vuln(address to) public returns(bool){
         return transfer(to, balanceOf(address(this)));
     }
-    //relations are not done yet
-    function transfer_vuln2(address to) public returns(bool){
-        uint256 balance = balanceOf(address(this));
-        return transfer(to, balance);
+
+    function transfer_vuln2(address to, uint256 amount) public returns(bool){
+        return transfer(to, amount);
     }
 
-    function transfer_ok(address to, uint256 amount) public returns(bool){
+    function transfer_ok(address to, uint256 amount) public onlyOwner returns(bool){
         return transfer(to, amount);
+    }
+}
+
+contract dangerous_balance_transfer_test {
+    IERC20 token;
+
+    function transfer_vuln3(address to, uint256 amount) public {
+        token.transfer(to, amount);
     }
 }
