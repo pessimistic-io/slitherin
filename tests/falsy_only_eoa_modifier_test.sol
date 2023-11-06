ff2e3ed0eccc30a656e680b99d4a3c832b8b0d1b
YanhuiJessica <137126578@qq.com>
2023-11-06 21:11:53 +0800

    fixed `IndexError`
    happens when `len(varListTx) > len(varListMsg)`

--- tests/falsy_only_eoa_modifier_test.sol
+++ tests/falsy_only_eoa_modifier_test.sol
@@ -2,15 +2,21 @@ pragma solidity ^0.8.0;
 
 contract falsy_only_eoa_modifier_test {
     uint256 toSet;
-    bool isProtected = true;
+    address owner = msg.sender;
 
     modifier onlyOwner() {
-        require(isProtected);
+        require(owner == msg.sender);
         _;
     }
 
-    function set_vulnurable(uint256 setter) public onlyOwner {
+    function set_vulnerable(uint256 setter) public onlyOwner {
         if(msg.sender == tx.origin){
+            toSet = setter;
+        }
+    }
+
+    function set_tx_origin(uint256 setter) public onlyOwner {
+        if(owner == tx.origin){
             toSet = setter;
         }
     }
