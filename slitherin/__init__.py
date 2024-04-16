fdec81b1816fe77cfcb72ddb5e83b4a81914bb90
olegggatttor <olegggatttor@gmail.com>
2024-04-16 14:27:04 +0300

    add vyper version detector

--- slitherin/__init__.py
+++ slitherin/__init__.py
@@ -32,6 +32,7 @@ from slitherin.detectors.arbitrum.block_number_timestamp import (
 from slitherin.detectors.arbitrum.arbitrum_chainlink_price_feed import ArbitrumChainlinkPriceFeed
 from slitherin.detectors.potential_arith_overflow import PotentialArithmOverflow
 from slitherin.detectors.curve.curve_readonly_reentrancy import CurveReadonlyReentrancy
+from slitherin.detectors.vyper.reentrancy_curve_vyper_version import CurveVyperReentrancy
 
 artbitrum_detectors = [
     ArbitrumPrevrandaoDifficulty,
@@ -65,6 +66,7 @@ plugin_detectors = artbitrum_detectors + [
     AAVEFlashloanCallbackDetector,
     PotentialArithmOverflow,
     CurveReadonlyReentrancy,
+    CurveVyperReentrancy
 ]
 plugin_printers = []
 
