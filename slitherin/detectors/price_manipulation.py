7669e6796247310310c49b054db1137bc174fa4e
olegggatttor <olegggatttor@gmail.com>
2024-04-16 14:35:09 +0300

    add file

---
+++ slitherin/detectors/price_manipulation.py
@@ -0,0 +1,37 @@
+from typing import List
+from slither.utils.output import Output
+from slither.detectors.abstract_detector import AbstractDetector, DetectorClassification
+from slither.core.declarations import Function
+from slither.slithir.operations.event_call import EventCall
+
+
+class PriceManipulationDetector(AbstractDetector):
+    """
+    Sees if contract setters do not emit events
+    """
+
+    ARGUMENT = 'pess-price-manipulation' # slither will launch the detector with slither.py --detect mydetector
+    HELP = 'Contract function does not emit event after the value is set'
+    IMPACT = DetectorClassification.LOW
+    CONFIDENCE = DetectorClassification.MEDIUM
+
+    WIKI = 'https://github.com/pessimistic-io/slitherin/blob/master/docs/event_setter.md'
+    WIKI_TITLE = 'Missing Event Setter'
+    WIKI_DESCRIPTION = "Setter-functions must emit events"
+    WIKI_EXPLOIT_SCENARIO = 'N/A'
+    WIKI_RECOMMENDATION = 'Emit events in setter functions'
+
+    def _detect(self) -> List[Output]:
+        """Main function"""
+        res = []
+        for contract in self.compilation_unit.contracts_derived:
+            if not contract.is_interface:
+                for f in contract.functions_and_modifiers_declared:
+                    if f.name.startswith("set"):
+                        x = self._emits_event(f)
+                        if not x:
+                            res.append(self.generate_result([
+                                "Setter function ",
+                                f, ' does not emit an event'
+                                '\n']))
+        return res
